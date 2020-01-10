51a47ea5d10016067f61755f2ff970e9347585e2
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2020-01-10 23:14:19 +0100

    Adds a few tests to ensure no breaking changes on dependency updates

---
+++ __tests__/minimal/index.jsx
@@ -0,0 +1,13 @@
+/**
+ * Tests the preset used without any options.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom/server';
+
+import Logo from './logo.svg';
+
+test('SVG import works as expected', () => {
+  const code = ReactDOM.renderToString(<Logo />);
+  expect(code).toMatchSnapshot();
+});
