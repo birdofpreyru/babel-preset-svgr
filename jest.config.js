a2032f52d2efc6cc74e1dc11cdad27cc2f983d07
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2020-03-19 21:55:05 +0100

    Fixes tests

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   testMatch: ['<rootDir>/__tests__/**/*.js?(x)'],
   testPathIgnorePatterns: [
     '/node_modules/',
+    '.babelrc.js',
   ],
   transform: {
     '\\.(jsx?|svg)$': ['babel-jest'],
